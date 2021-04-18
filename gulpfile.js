1c6d8955a94c09f46f81f86de0b8242db071d102
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2021-04-18 21:31:22 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,41 @@
 "use strict";
-let gulp = require('gulp');
-let sass = require('gulp-sass'); //Компилятор SCSS --> CSS
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-let autoPrefix = require('gulp-autoprefixer'); //Расстановка префиксов
-let cleanCSS = require('gulp-clean-css'); //Минификация CSS
-let uglify = require('gulp-uglify'); //Обычная минификация JS
-let rename = require('gulp-rename'); //Перетменовае файлов
-let del = require('del'); //Очистка
-let browserSync = require('browser-sync').create(); //Локальный сервер
-const webpack = require("webpack-stream"); //Сборка модулей
+const autoPrefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const webpack = require('webpack-stream');
 
-/*==================================================================================*/
-const dist = "./dist";
+const dist = './dist';
 
-//Html
-function layoutHTML() {
-  return (gulp.src('./src/*.html'))
-      .pipe(gulp.dest('./dist/'))
+//Copy HTML
+function html() {
+  return gulp.src('./src/*.html')
+      .pipe(gulp.dest(dist))
       .pipe(browserSync.reload({stream: true}))
 }
 
 //Styles
 function style() {
-  return gulp.src('./src/scss/style.scss')
-  //Компиляция SCSS to CSS
+  return gulp.src('./src/sass/style.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      //Растановка префиксов
-      .pipe(autoPrefix({
-        overrideBrowserslist: ['last 2 versions'],
+      .pipe(autoPrefixer({
+        overrideBrowserList: ['last 2 versions'],
         cascade: false
       }))
-      //Минификация
-      .pipe(cleanCSS({
-        level: 2
-      }))
-      //Переименование
+      .pipe(cleanCSS({level: 2}))
       .pipe(rename({suffix: '.min'}))
-      //Папка назначения
-      .pipe(gulp.dest('./dist/css'))
+      .pipe(gulp.dest(dist + '/css'))
       .pipe(browserSync.reload({stream: true}))
 }
 
-//JavaScript
-// Webpack Development
-function buildJS() {
-  return gulp.src("./src/js/main.js")
-      .pipe(webpack({
-        mode: 'development',
-        output: {
-          filename: 'index.js'
-        },
-        module: {
-          rules: [
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [['@babel/preset-env', {
-                    debug: true,
-                    corejs: 3,
-                    useBuiltIns: "usage"
-                  }]]
-                }
-              }
-            }
-          ]
-        }
-      }))
-      .pipe(uglify())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(dist + '/js'))
-      .pipe(browserSync.reload({stream: true}))
-}
-
-// Webpack Production
-gulp.task("build-prod-js", () => {
-  return gulp.src("./src/js/main.js")
+//JS
+function scripts() {
+  return gulp.src('./src/js/main.js')
       .pipe(webpack({
         mode: 'production',
         output: {
@@ -87,13 +45,14 @@ gulp.task("build-prod-js", () => {
           rules: [
             {
               test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
+              exclude: /(node_modules)/,
               use: {
                 loader: 'babel-loader',
                 options: {
                   presets: [['@babel/preset-env', {
+                    debug: false,
                     corejs: 3,
-                    useBuiltIns: "usage"
+                    useBuiltIns: 'usage'
                   }]]
                 }
               }
@@ -105,39 +64,37 @@ gulp.task("build-prod-js", () => {
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest(dist + '/js'))
       .pipe(browserSync.reload({stream: true}))
-});
+}
 
-//Просматривать файлы
+//Images
+function img() {
+  return gulp.src("./src/img/**/*.*")
+      .pipe(gulp.dest(dist + '/img'))
+}
+
+// Fonts
+function fonts() {
+  return gulp.src('./src/fonts/**/*.*')
+      .pipe(gulp.dest(dist + '/fonts'))
+}
+
+// Clean
+function clean() {
+  return del([dist])
+}
+
+// Watch
 function watch() {
   browserSync.init({
     server: {
       baseDir: dist
     }
   });
-  gulp.watch('./src/*.html', layoutHTML);
+  gulp.watch('./src/*.html', html);
   gulp.watch('./src/sass/**/*.scss', style);
-  gulp.watch('./src/js/*.js', buildJS);
+  gulp.watch('./src/js/*.js', scripts);
 }
 
-//Копирование изображений и шрифтов из src
-function copyImg() {
-  return gulp.src("./src/img/**/*.*")
-      .pipe(gulp.dest('./dist/img/'))
-}
+gulp.task('build', gulp.series(clean, gulp.parallel(html, style, scripts, img, fonts)));
 
-function copyFonts() {
-  return gulp.src("./src/fonts/**/*.*")
-      .pipe(gulp.dest('./dist/fonts/'))
-}
-
-//Очитка папки dist
-function clean() {
-  return del(['./dist/*'])
-}
-
-//Таск для удаления файлов в папке dist и запуск сборки
-gulp.task('build', gulp.series(clean, gulp.parallel(layoutHTML, style, buildJS, copyImg, copyFonts)));
-
-//Таск запускает таск build и watch последовательно
 gulp.task('default', gulp.series('build', watch));
-
